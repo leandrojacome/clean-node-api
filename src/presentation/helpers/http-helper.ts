c2f7c7444791eaf0015733f6c4463fe5a839a373
Leandro Rodrigues Jácome <leandro@codespec.com.br>
2023-05-14 15:41:41 -0300

    refator: simplify code validation

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,19 +4,7 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
-export const serverError = (error: Error): HttpResponse => ({
-  statusCode: 500,
-  body: error
-})
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
-})
-export const unauthorized = (error: Error): HttpResponse => ({
-  statusCode: 401,
-  body: error
-})
-export const forbidden = (error: Error): HttpResponse => ({
-  statusCode: 403,
-  body: error
 })
