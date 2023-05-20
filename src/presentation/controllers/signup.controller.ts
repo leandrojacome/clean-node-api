f52ccd9f7f366936db03ec85510381831e5c8b10
Leandro Rodrigues Jácome <leandro@codespec.com.br>
2023-05-20 20:45:25 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws Exception

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from '../erros/invalid-param-error'
 import { MissingParamError } from '../erros/missing-param-error'
+import { ServerError } from '../erros/server-error'
 import { badRequest, ok } from '../helpers/http-helper'
 import { type Controller } from '../protocols/controller'
 import { type EmailValidator } from '../protocols/email-validator'
@@ -8,15 +9,22 @@ import { type HttpRequest, type HttpResponse } from '../protocols/http'
 export class SignUpController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {}
   handle (httpRequest: HttpRequest): HttpResponse {
-    const fieldsValidation = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of fieldsValidation) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const fieldsValidation = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of fieldsValidation) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+      if (!this.emailValidator.isValid(httpRequest.body.email)) {
+        return badRequest(new InvalidParamError('email'))
+      }
+      return ok({})
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
       }
     }
-    if (!this.emailValidator.isValid(httpRequest.body.email)) {
-      return badRequest(new InvalidParamError('email'))
-    }
-    return ok({})
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,6 +4,10 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: error
+})
 export const ok = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
